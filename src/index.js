64ae84d99fe1a231d5159ee8aaff9018cc5cbe86
meerajahmed <meerajahmed@gmail.com>
2018-01-11 00:14:46 +0530

    resubscribing to a stream with repeat

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import "rxjs/add/operator/filter";
 import "rxjs/add/operator/takeWhile";
 import "rxjs/add/operator/reduce";
 import "rxjs/add/operator/withLatestFrom";
+import "rxjs/add/operator/repeat";
 
 const startButton = document.querySelector('#start');
 const halfButton = document.querySelector('#half');
@@ -248,7 +249,7 @@ Observable.combineLatest(
 /****************************** competing stream withLatestfrom**************************************/
 
 
-timer$
+/*timer$
   .do((x) => console.log("Timer :",x))
   .takeWhile(data => data.count <= 3)
   // takeWhile will complete stream at tick 4
@@ -260,7 +261,7 @@ timer$
   inputText$.do((x) => console.log("Input :",x)),
   (timer, input) => ({count: timer.count, text: input}))
 // do -> something that is going to happen outside the stream
-  .do((x) => console.log("combineLatest :",x))
+  .do((x) => console.log("withLatestFrom :",x))
   //filters don't complete the stream. Filter just tells our streams which things to push through
   .filter((data) => data.count === parseInt(data.text))
   // calculate final score with reduce
@@ -273,4 +274,35 @@ timer$
     x => console.log("Total score :", x),
     err => console.log(err),
     () => console.log("Game Over !!!")
+  );*/
+
+/****************************** resubscribing to a stream with repeat ***************************/
+
+timer$
+  .do((x) => console.log("Timer :",x))
+  .takeWhile(data => data.count <= 3)
+  // takeWhile will complete stream at tick 4
+  //.combineLatest(
+  //combineLatest waits for complete event from both timer and input.
+  // But input never completes
+  .withLatestFrom(
+    // timer will take latest value from input, but it wont wait for the the input to complete
+    inputText$.do((x) => console.log("Input :",x)),
+    (timer, input) => ({count: timer.count, text: input}))
+  // do -> something that is going to happen outside the stream
+  .do((x) => console.log("withLatestFrom :",x))
+  //filters don't complete the stream. Filter just tells our streams which things to push through
+  .filter((data) => data.count === parseInt(data.text))
+  // calculate final score with reduce
+  // reduce collects data until stream hits complete
+  .reduce((acc, curr) => acc + 1, 0) // acc  -> tick, data from filter is passed with curr
+  // reduce operator runs on complete -
+  // subscribe block is now waiting for complete event, final output
+  .repeat() // usually used before subscribe block
+  // resubscribe on complete event from reduce but does not completes subscriber
+  .subscribe(
+    // next: called on every tick
+    x => console.log("Total score :", x),
+    err => console.log(err),
+    () => console.log("Game Over !!!") // after using repeat, we dont get complete event
   );
